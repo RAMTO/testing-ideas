dd3f8b736068eeb1684faefa3c91119791422079
Martin Dobrev <martindobrev87@gmail.com>
2018-10-10 22:11:59 +0300

    Updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,46 @@
-const form = document.querySelector('#cta-form');
-
-function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+function validateEmail (email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(email).toLowerCase())
 }
 
-form.addEventListener('submit', (e) => {
-  const email = form.querySelector('#mce-EMAIL');
-  const value = email.value.trim();
+const $mainForm = $('#mainForm')
+const $type = $('#type')
+const $buttonType = $('.button-type')
+const $cardFeatures = $('.card-feature')
+const $stateForm = $('.state-form')
+const $stateSuccess = $('.state-success')
 
-  if (validateEmail(value)) {
-    email.classList.remove('is-invalid');
-  } else {
-    e.preventDefault();
-    email.classList.add('is-invalid');
+let formValid = true
+
+$mainForm.on('submit', function (e) {
+  $this = $(this)
+  $inputs = $this.find('input')
+
+  e.preventDefault()
+
+  $inputs.each(function (index, input) {
+    const $input = $(input)
+    const value = $input.val().trim()
+
+    if (value === '') {
+      formValid = false
+    }
+  })
+
+  if (formValid) {
+    $.post($this.attr('action'), $this.serialize()).then(function () {
+      $stateForm.hide()
+      $stateSuccess.show()
+    })
   }
-});
+})
+
+$buttonType.on('click', function () {
+  const $this = $(this)
+  const type = $this.data('type')
+  const $parent = $this.parents('.card-feature')
+
+  $cardFeatures.removeClass('is-active')
+  $parent.addClass('is-active')
+  $type.prop('value', type)
+})
